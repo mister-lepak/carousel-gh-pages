0825af42927a6aeee64b92f9318c077dcf07328e
Try Wijono <mrlepak@users.noreply.github.com>
2021-02-06 16:59:24 +0800

    Add files via upload

---
+++ precache-manifest.375f362d6642fa5ffbd5913dc97c0b1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fbcd37b19c570e96715b5fcfdb3861",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cee6061b568823045591",
+    "url": "./static/css/main.8a6380e9.chunk.css"
+  },
+  {
+    "revision": "965a0a678b16fd661f6b",
+    "url": "./static/js/2.2f9d683c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2f9d683c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee6061b568823045591",
+    "url": "./static/js/main.66be3515.chunk.js"
+  },
+  {
+    "revision": "ed44975e28af9b70facf",
+    "url": "./static/js/runtime-main.a08d65c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
